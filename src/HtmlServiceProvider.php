86f3d827cebccaecb2f4e7919d8c3dd67d852b89
Renato Dehnhardt <renatotkd@gmail.com>
2017-06-05 22:45:00 -0300

    Update HtmlServiceProvider.php

--- src/HtmlServiceProvider.php
+++ src/HtmlServiceProvider.php
@@ -13,7 +13,7 @@ class HtmlServiceProvider extends IlluminateHtmlServiceProvider
     protected function registerFormBuilder()
     {
         $this->app->bind('form', function ($app) {
-            $form = new FormBuilder($app['html'], $app['url'], $app['view'], $app['session.store']->getToken());
+            $form = new FormBuilder($app['html'], $app['url'], $app['view'], $app['session.store']->token());
 
             return $form->setSessionStore($app['session.store']);
         });
